0de84fa4bd74c0cbb2b044f3845883709d84ed3f
Mehdi-boullouf <43892500+Mehdi-boullouf@users.noreply.github.com>
2018-10-06 15:18:22 +0200

    Add files via upload

---
+++ Dz.py
@@ -0,0 +1,128 @@
+#First ScriPt I maket By My self
+
+
+print("########################################################")
+print("&                                                      &")
+print("&                  By Mehdi Boullouf                   &")
+print("&                                                      &")
+print("########################################################")
+print("|                                                      | ")
+print("|                                                      | ")
+print(" ###########{special Thanks To Emad Fakhry}############")
+kali  = "www.kali.org/downloads>> if You Want To be an Big Hacker use it"
+sqlmap = "it is an tool for hacking websites >>Sql injection And You can Learn Using it in termux"
+armitage = "it is An Tool for Hacking Devices >>any Device And You Can Using Him in Kali linux"
+easy_hack = "big tool and you can download it >>git clone https://github.com/sabri-zaki/Easy_Hack"
+teach_python = "Madrasa Tech Officiel"
+teach_html = "contact me i Teach You everything About Html"
+channels_python = "madrasa Tech officiel,isecurity1"
+websites_python = "harmash,python"
+kali_lessons = "project11"
+books_python_hack = "Black Hat python"
+wep_wifi_hack = "just write admin and will connect but it is failed because No internrt just connected to network"
+wps_wifi_hack = "Wps Wpa Tester,androdumper,wps connect"
+hydra = "an tool for hacking Facebook and gmail but T5mine only"
+best_group_teaching_termux = "@Termux@"
+ddos_attacking = "hummer"
+start_termux = "First Ever : Dwonload Python2>>pkg install python2 Second<<Second If You are Want to HAcking Websites Download Sqlmap , if You Want HAcking Device connected For some network Download Xerosploit"
+way_hack_facebook = "first ever: Fake pages you can use : shellphish,weeman second: Password Attacks Three: DArk Web You can HAck it for 4 Dollars and maybe You can hack it By acces token using osif tool"
+way_hack_pc = "many many Tools In Windows : Bifrost , njrat ,Turkojan,..... in kali armitage is the best tool "
+free_internet_android = "Yoga vpn"
+money = "Contact Me i will show you A Secret Way"
+wifi = "Just write The security Example wps write :wps_wif_hack"
+mehdi_boullouf = "Iam From Algeria iam 14 years Old Ilove Programming So much and iam professional in html and javascrip and beginner in python"
+ipgeolocation = "This Tool is Amazing Because Give You ,Location,Date Of created,and many informations"
+weeman_install = "git clone https://github.com/evait-security/weeman>>chmod 777 weeman.py"
+ngrok = "open port in Network>>git clone https://github.com/themastersunil/ngrok>>chmod 777 ngrok"
+books_kali = "https://www.hacker-ar.com/2017/10/kali-linux.html"
+sql = "The best Way is sQlmap in Linux and termux,HAvij in Windows"
+bypass = "Dork>>admin/login.asp exploit>>admin in usernam >>admin password \"or"
+print("\n")
+choose = input("Enter Any Thing You Want To Get Info >> \mehdi~# ")
+if choose == "kali":
+	print (kali)
+elif choose == "sqlmap":
+	print (sqlmap)
+elif choose == "armitage":
+	print (armitage)
+elif choose == "easy_hack":
+	print (easy_hack)
+elif choose == "tech_python":
+	print (tech_python)
+elif choose == "tech_html":
+	print (tech_html)
+elif choose == "channels_python":
+	print (channels_python)
+elif choose == "websites_python":
+	print (websites_python)
+elif choose == "kali_lessons":
+	print (kali_lessons)
+elif choose == "books_python_hack":
+	print (books_python_hack)
+elif choose == "wep_wifi_hack":
+	print (wep_wifi_hack)
+elif choose == "wps_wifi_hack":
+	print (wps_wifi_hack)
+elif choose == "hydra":
+	print (hydra)
+elif choose == "best_group_teaching_termux":
+	print (best_group_teaching_termux)
+elif choose == "ddos_attacking":
+	print (ddos_attacking)
+elif choose == "start_termux":
+	print (start_termux)
+elif choose == "way_hack_facebook":
+	print (way_hack_facebook)
+elif choose == "way_hack_pc":
+	print (way_hack_pc)
+elif choose == "free_internet_android":
+	print (free_internet_android)
+elif choose == "wifi":
+	print (wifi)
+elif choose == "money":
+	print (money)
+elif choose == "mehdi_boullouf":
+	print (mehdi_boullouf)
+elif choose == "ipgeolocation":
+	print (ipgeolocation)
+elif choose == "weeman_install":
+	print (weeman_install)
+elif choose == "ngrok":
+	print (ngrok)
+elif choose == "books_kali":
+             print (kali)
+elif choose == "sql":
+             print (sql)
+elif choose == "bypass":
+             print (bypass)
+
+
+
+elif choose == "help":
+        print ("kali")
+        print ("sqlmap ")
+        print ("armitage")
+        print ("easy_hack")
+        print ("teach_python")
+        print ("teach_html")
+        print ("channels_python")
+        print ("websites_python")
+        print ("kali_lessons")
+        print ("books_python_hack")
+        print ("start_termux")
+        print ("way_hack_facebook")
+        print ("free_internet_android")
+        print ("money")
+        print ("wifi")
+        print ("ipgeolocation")
+        print ("weeman_install")
+        print ("books_kali")
+        print ("ngrok")
+        print ("sql")
+        print ("bypass")
+
+
+
+else: print ("Invalid Choose !")
+
+
